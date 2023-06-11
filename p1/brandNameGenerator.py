72bb53a764acda97e47935a662dd665046d6af00
naim71 <naimkhan1314@gmail.com>
2023-06-11 14:50:16 +0600

    first commit

---
+++ p1/brandNameGenerator.py
@@ -0,0 +1,6 @@
+print("Welcome to the Brand name Geneartor")
+
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's the name of your first pet?\n")
+
+print("Your band name should be: "+ city + "" + pet)
---
+++ p1/day2/d2.py
@@ -0,0 +1,7 @@
+num = input("Type a two digit number:\n")
+num1 = num[0]
+num2 = num[1]
+
+result = int(num1) + int(num2)
+
+print(result)
